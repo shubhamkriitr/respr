a030dcf456e5f0c0dc8bba5f6a455181a3aaaf60
Shubham Kumar <kumarsh@student.ethz.ch>
2022-11-16 16:32:12 +0100

    add pytorch dataset and loader

--- respr/data/loader.py
+++ respr/data/loader.py
@@ -1,7 +1,10 @@
 from torch.utils.data import Dataset, DataLoader
 from respr.data.base import BaseDataAdapter
 from sklearn.model_selection import train_test_split
+import pandas as pd
+import numpy as np
 
+DTYPE_FLOAT = np.float32
 # TODO: set seeds for random 
 
 def create_train_val_test_split(sample_ids: list, val=0.2, test=0.2):
@@ -61,9 +64,55 @@ class ResprDataset(Dataset):
         pass
         
 
+
+class BaseResprCsvDataset(Dataset):
+    def __init__(self, config={}) -> None:
+        super().__init__()
+        self._config = config
+        self._dataset_path = self._config["dataset_path"]
+        self.num_samples = None
+        self.initialize_source()
+    
+    def initialize_source(self):
+        """Read the whole dataset or do setup to read the data from disk on
+        demand. Also do an initial scan to setup size (`num_samples`) and
+        index for index base access."""
+        df = pd.read_csv(self._dataset_path)
+        cols = df.keys()
+        self.x_cols = sorted([c for c in cols if c.startswith("x_")])
+        x_cols_idx = [int(c.split("x_")[1]) for c in self.x_cols]
+        x_idx_0 = x_cols_idx[0]
+        assert x_idx_0 == 0
+        x_idx_max = x_cols_idx[-1]
+        assert (x_idx_max + 1) == len(x_cols_idx)
+        
+        self.num_samples = df["y"].shape[0]
+        
+        self.data = df
+         
+
+    
+    def __len__(self) -> int:
+        return self.num_samples
+
+    def __getitem__(self, index: int):
+        x, y = self.data.loc[index, self.x_cols], self.data.loc[index, "y"]
+        x = x.to_numpy()
+        
+        return x, y
+    
     
     
 if __name__ == "__main__":
+    
+    ds = BaseResprCsvDataset({"dataset_path": \
+        "../artifacts/2022-11-16_161146/dataset.csv"})
+    
+    dl = DataLoader(dataset=ds, batch_size=3)
+    
+    for batch in dl:
+        b = batch
+        print(type(b))
     sample_ids = [i for i in range(10)]
     train_samples, val_samples, test_samples = \
         create_train_val_test_split(sample_ids, 0.2, 0.2)
