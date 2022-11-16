c31f45a51314f31358d2b7458a30fd73bd279507
Shubham Kumar <kumarsh@student.ethz.ch>
2022-11-16 16:54:20 +0100

    restructure

--- respr/pipeline/base.py
+++ respr/pipeline/base.py
@@ -697,17 +697,6 @@ REGISTERED_PIPELINES = {
     "DatasetBuilder": DatasetBuilder
 }  
 
-if __name__ == "__main__":
-    import yaml
-    DEFAULT_PIPELINE = "Pipeline2"
-    config_path = DEFAULT_CONFIG_PATH
-    config_data = None
-    with open(config_path, 'r', encoding="utf-8") as f:
-        config_data = yaml.load(f, Loader=yaml.FullLoader)
-    pipeline_name = config_data["pipeline"]["name"]
-    logger.info(f"Using pipeline: {pipeline_name}")
-    if pipeline_name is None:
-        pipeline_name = DEFAULT_PIPELINE
-    pipeline_class = REGISTERED_PIPELINES[pipeline_name]
-    p = pipeline_class(config_data)
-    p.run()
+
+
+
---
+++ respr/pipeline/train.py
@@ -0,0 +1,3 @@
+from respr.core.ml.models.mlp import LitResprVanillaMLP
+import pytorch_lightning as pl
+
