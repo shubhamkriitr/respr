52ee1ab144ded45a355afecd9c0e01e958185be9
Shubham Kumar <kumarsh@student.ethz.ch>
2023-01-04 01:10:46 +0100

    add workaround file

---
+++ respr/eval/_results_file_paths.py
@@ -0,0 +1,10 @@
+# TODO: use yaml or json file to store these paths. This is a etmporary
+# workaround. Check : `_results_file_paths.py` in `respr-notebooks` repository  
+# https://gitlab.ethz.ch/kumarsh/respr-notebooks/-/blob/c13430ad46894f32015d00a568a77cb919ecd8d1/_results_file_paths.py
+from pathlib import Path
+
+
+experiment_num_to_result = {
+    
+
+}
