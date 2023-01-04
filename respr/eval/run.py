76134b949374823800188176a4e0036ea3e9d3c0
Shubham Kumar <kumarsh@student.ethz.ch>
2023-01-04 01:15:33 +0100

    update eval run.py

--- respr/eval/run.py
+++ respr/eval/run.py
@@ -1,4 +1,21 @@
 
+import pickle
+import json
+import yaml
+import pandas as pd
+import heartpy
+from pathlib import Path
+import os
+from loguru import logger
+import re
+import heartpy as hp
+from scipy import signal
+import scipy
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import textwrap
+
 from respr.pipeline.base import BasePipeline
 from respr.util.common import logger
 from respr.eval.evaluation import (TYPE_PNN,
@@ -28,10 +45,56 @@ def gather_results_from_source(results_source, loaders=DEFAULT_LOADER_MAPPING):
         all_model_results.append((data, type_code, tag))
     
     return all_model_results, loocv_fold_wise_metric
+
+def convert_loocv_fold_wise_metric_to_df(loocv_fold_wise_metric, fold_numbers):
+
+    df = [np.expand_dims(np.array(fold_numbers), 1)]
+    df_cols = ["fold"]
+    tags = sorted(list(loocv_fold_wise_metric.keys()))
+    for tag in tags:
+        d = loocv_fold_wise_metric[tag]
+        folds = set([int(f) for f in d])
+        mae_list = []
+        rmse_list = []
+        mean_std_list = []
+        for fold in fold_numbers:
+            if fold not in folds:
+                rmse = np.nan
+                mae = np.nan
+                mean_std = np.nan
+            else:
+                fold_str = str(fold).zfill(4)
+                rmse = d[fold_str]['rmse']['[metric_rmse]gt:rr_est_pnn']
+                mae = d[fold_str]["mae"]["[metric_mae]gt:rr_est_pnn"]
+                mean_std = d[fold_str]["mean_std"]
+            
+            mae_list.append(mae)
+            rmse_list.append(rmse)
+            mean_std_list.append(mean_std)
+        
+        mae_arr = np.expand_dims(np.array(mae_list), 1)
+            
+        rmse_arr = np.expand_dims(np.array(rmse_list), 1)
+        mean_std_arr = np.expand_dims(np.array(mean_std_list), 1)
+        
+        df.append(mae_arr)
+        df_cols.append(f"{tag}_mae")
+        df.append(rmse_arr)
+        df_cols.append(f"{tag}_rmse")
+        df.append(mean_std_arr)
+        df_cols.append(f"{tag}_mean_std")
+    
+    df = np.concatenate(df, axis=1)
+    df = pd.DataFrame(df, columns=df_cols)
+    
+    return df
+
 class EvalPipeline(BasePipeline):
     def __init__(self, config={}) -> None:
         super().__init__(config)
     
     
     
+if __name__ == "__main__":
+    from ._results_file_paths import experiment_num_to_result
     
